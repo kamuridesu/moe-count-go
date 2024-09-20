74e89e62a46da838ec2fe701f45301252f298e5a
Kamuri Amorim <myk.gata14@gmail.com>
2024-09-20 18:45:11 -0300

    fix: use gomechan utilities

--- go.mod
+++ go.mod
@@ -1,9 +1,11 @@
 module github.com/kamuridesu/moe-count-go
 
-go 1.21
+go 1.22.5
 
 require (
 	github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b
 	github.com/lib/pq v1.10.9
 	github.com/mattn/go-sqlite3 v1.14.19
 )
+
+require github.com/kamuridesu/gomechan v0.0.0-20240920201250-c7df6aa0ad22
--- server.go
+++ server.go
@@ -3,106 +3,28 @@ package main
 import (
 	"errors"
 	"fmt"
-	"html/template"
-	"io"
 	"log/slog"
 	"net/http"
 	"os"
-	"path"
-	"strings"
-	"time"
+
+	"github.com/kamuridesu/gomechan/core/response"
+	"github.com/kamuridesu/gomechan/core/templates"
 )
 
-func loadTemplate(filename string) (string, error) {
-	templateFolderName := "template"
-	templateFolder, err := os.ReadDir(templateFolderName)
-	if err != nil {
-		return "", err
-	}
-	for _, file := range templateFolder {
-		if file.Name() == filename {
-			content, err := os.ReadFile(path.Join(templateFolderName, file.Name()))
-			if err != nil {
-				slog.Error(fmt.Sprintf("Error loading %s: %s", filename, err))
-			}
-			return string(content), nil
-		}
-	}
-	return "", fmt.Errorf("template %s not found in template folder", filename)
-}
-
-func parseTemplate(filename string, variables map[string]interface{}) (string, error) {
-	content, err := loadTemplate(filename)
-	if err != nil {
-		return "", err
-	}
-
-	tmpl, err := template.New("template").Parse(content)
-	if err != nil {
-		return "", fmt.Errorf("error parsing template: %s", err)
-	}
-
-	buff := new(strings.Builder)
-
-	err = tmpl.Execute(buff, variables)
-	if err != nil {
-		return "", err
-	}
-	return buff.String(), nil
-}
-
-type ResponseWriter struct {
-	startTimeRequest time.Time
-	w                *http.ResponseWriter
-	r                *http.Request
-	status           int
-	body             string
-	headers          map[string]string
-}
-
-func NewResponseWriter(w *http.ResponseWriter, r *http.Request) ResponseWriter {
-	return ResponseWriter{
-		startTimeRequest: time.Now(),
-		status:           http.StatusOK,
-		headers:          map[string]string{},
-		body:             "",
-		w:                w,
-		r:                r,
-	}
-}
-
-func (r *ResponseWriter) Build(body string, status int, headers map[string]string) {
-	r.body = body
-	r.status = status
-	r.headers = headers
-}
-
-func (r *ResponseWriter) BuildAndSend(body string, status int, headers map[string]string) {
-	r.Build(body, status, headers)
-	r.Send()
-}
-
-func (r *ResponseWriter) Send() {
-	for k, v := range r.headers {
-		(*r.w).Header().Add(k, v)
-	}
-	(*r.w).WriteHeader(r.status)
-	io.WriteString((*r.w), r.body)
-	requestTime := time.Since(r.startTimeRequest)
-	slog.Info(fmt.Sprintf("| %-3d | %-12v | %-15s | %-6s | %-30s",
-		r.status, requestTime, strings.Split(r.r.RemoteAddr, ":")[0], r.r.Method, r.r.URL))
-}
+var (
+	Templates, _ = templates.LoadTemplateFolder("./template")
+)
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	responseWriter := NewResponseWriter(&w, r)
+	responseWriter := response.New(&w, r)
 	var headers map[string]string
 
 	if r.URL.Path != "/" {
 		headers = map[string]string{
 			"content-type": "text/html",
 		}
-		notFound, _ := parseTemplate("404.tmpl", map[string]interface{}{"message": "Error! Page not found"})
-		responseWriter.BuildAndSend(notFound, http.StatusNotFound, headers)
+		notFound := Templates.LoadHTML("404.tmpl", map[string]interface{}{"message": "Error! Page not found"})
+		responseWriter.BuildAndSend(http.StatusNotFound, notFound, headers)
 		return
 	}
 
@@ -112,8 +34,8 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 			"x-missing-field": "username",
 			"content-type":    "text/html",
 		}
-		notFound, _ := parseTemplate("404.tmpl", map[string]interface{}{"message": "Error! Missing username param"})
-		responseWriter.BuildAndSend(notFound, http.StatusBadRequest, headers)
+		notFound := Templates.LoadHTML("404.tmpl", map[string]interface{}{"message": "Error! Page not found"})
+		responseWriter.BuildAndSend(http.StatusBadRequest, notFound, headers)
 		return
 	}
 
@@ -125,10 +47,9 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		user, err = mainDatabase.insertUserIntoDB(username)
 		if err != nil {
-			headers = map[string]string{
-				"content-type": "application/json",
-			}
-			responseWriter.BuildAndSend(fmt.Sprintf("{\"message\": \"%s\"}", err), http.StatusInternalServerError, headers)
+			responseWriter.AsJson(http.StatusInternalServerError, map[string]any{
+				"message": err.Error(),
+			})
 			return
 		}
 	}
@@ -141,12 +62,12 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 		"content-type":  "image/svg+xml",
 	}
 	mainDatabase.updateUserViewCount(user)
-	responseWriter.BuildAndSend(generateSVG(user.counter, loadedImages).String(), http.StatusOK, headers)
+	responseWriter.BuildAndSend(http.StatusOK, generateSVG(user.counter, loadedImages).String(), headers)
 }
 
 func healthCheck(w http.ResponseWriter, r *http.Request) {
-	writer := NewResponseWriter(&w, r)
-	writer.BuildAndSend("{\"status\": \"up\"}", http.StatusOK, map[string]string{"Content-Type": "application/json"})
+	writer := response.New(&w, r)
+	writer.BuildAndSend(http.StatusOK, "{\"status\": \"up\"}", map[string]string{"content-type": "application/json"})
 }
 
 func serve() {
